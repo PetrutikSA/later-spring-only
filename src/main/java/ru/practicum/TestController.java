8477e49a42350788a4a77ebc7da9e61ce3f3a83e
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-02 16:03:23 +0300

    feat: add logging

--- src/main/java/ru/practicum/TestController.java
+++ src/main/java/ru/practicum/TestController.java
@@ -1,13 +1,17 @@
 package ru.practicum;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    public static final Logger logger= LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
     public String test() {
+        logger.trace("get request");
         return "OK!";
     }
 }
