05f27bc7f215da480a650bbc0b1e517a94ec4f05
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-20 17:02:50 +0300

    feat: add ORM annotations

--- src/main/java/ru/practicum/item/Item.java
+++ src/main/java/ru/practicum/item/Item.java
@@ -1,15 +1,20 @@
 package ru.practicum.item;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 @Entity
+@Table(name = "items")
+@Getter
+@Setter
 public class Item {
     @Id
     private Long id;
-
+    @Column(name="user_id")
     private Long userId;
     private String url;
 }
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -1,10 +1,22 @@
 package ru.practicum.user;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
+    @Id
     private Long id;
     private String email;
     private String name;
+    @Enumerated(EnumType.STRING)
+    private UserState userState;
 }
