9e3ec04b811689d0864ae2b069b4e52ba3ff22de
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-20 16:58:41 +0300

    feat: add UserState

--- src/main/java/ru/practicum/item/Item.java
+++ src/main/java/ru/practicum/item/Item.java
@@ -1,10 +1,15 @@
 package ru.practicum.item;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Item {
+    @Id
     private Long id;
+
     private Long userId;
     private String url;
 }
