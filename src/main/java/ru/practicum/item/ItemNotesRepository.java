34c460a4a64e1da0dc3ba20aacb9ea0344b031a6
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-26 17:10:05 +0300

    feat: add ItemNoteRepository logic

--- src/main/java/ru/practicum/item/ItemNotesRepository.java
+++ src/main/java/ru/practicum/item/ItemNotesRepository.java
@@ -1,8 +1,20 @@
 package ru.practicum.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemNotesRepository extends JpaRepository<ItemNote, Long> {
+    List<ItemNote> findAllByItemUserIdAndItemUrlContainingIgnoreCase (Long userId, String url);
+
+    @Query("""
+            select notes
+            from ItemNotes as notes
+            join notes.item as i
+            where i.user_id = ?1 and ?2 member of i.tags
+            """)
+    List<ItemNote> findItemNotesByUserIdAndTag (Long userId, String tag);
 }
