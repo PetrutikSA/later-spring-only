189443ed2b23fde262cd401721bc5b9eb0456572
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-09-05 11:17:58 +0300

    feat: correct save new item logic

--- src/main/java/ru/practicum/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/item/ItemServiceImpl.java
@@ -2,13 +2,17 @@ package ru.practicum.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.item.metadata.UrlMetadata;
+import ru.practicum.item.metadata.UrlMetadataRetrieverImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl  implements ItemService{
     private final ItemRepository itemRepository;
+    private final UrlMetadataRetrieverImpl urlMetadataRetriever;
 
     @Override
     public List<Item> getAllItems(long userId) {
@@ -17,7 +21,29 @@ public class ItemServiceImpl  implements ItemService{
 
     @Override
     public Item saveNewItem(long userId, Item item) {
-        return itemRepository.save(item);
+        item.setUserId(userId);
+
+        UrlMetadata urlMetadata = urlMetadataRetriever.retrieve(item.getUrl());
+
+        Optional<Item> maybeExistingItem =
+                itemRepository.findByUserIdAndResolvedUrl(userId, urlMetadata.getResolvedUrl());
+
+        if (maybeExistingItem.isEmpty()) {
+            item.setTitle(urlMetadata.getTitle());
+            item.setResolvedUrl(urlMetadata.getResolvedUrl());
+            item.setMimeType(urlMetadata.getMimeType());
+            item.setHasImage(urlMetadata.isHasImage());
+            item.setHasVideo(urlMetadata.isHasVideo());
+            item.setDateResolved(urlMetadata.getDateResolved());
+            item = itemRepository.save(item);
+        } else {
+            Item itemFromDB = maybeExistingItem.get();
+            if(item.getTags() != null && !item.getTags().isEmpty()) {
+                itemFromDB.getTags().addAll(item.getTags());
+                item = itemRepository.save(itemFromDB);
+            }
+        }
+        return item;
     }
 
     @Override
